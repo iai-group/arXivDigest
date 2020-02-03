eb9734094f2ee65fd34d63ecdf01681a9b42a983
kgingstad <kristiangingstad@gmail.com>
2020-02-03 19:41:31 +0100

    Message when loading keywords

--- frontend/static/javascript/keywords.js
+++ frontend/static/javascript/keywords.js
@@ -1,9 +1,11 @@
 function fetch_suggested_keywords(value){
+    $("#keyword_info").html("Loading suggested keywords...");
     $.getJSON("/author_keywords/" + value, {},
         function (data) {
             if (data.keywords != "" || data.keywords != null) {
                 $.each(data.keywords, append_keywords); //ajax result contains a list of keywords (top 8 shown on website)
             };
+            $("#keyword_info").html("");
         });
 };
 
