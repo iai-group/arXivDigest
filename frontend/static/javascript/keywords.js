e12dcca4146cc3523a1f2f4ced871cf0cebbcdcb
kgingstad <kristiangingstad@gmail.com>
2020-02-09 23:37:26 +0100

    dict + sorting of keywords

--- frontend/static/javascript/keywords.js
+++ frontend/static/javascript/keywords.js
@@ -4,7 +4,7 @@ function fetch_suggested_keywords(value) {
     $.getJSON("/author_keywords/" + value, {},
         function (data) {
             if (data.error == null) { 
-                $.each(data.keywords, append_keywords); //ajax result contains a list of keywords (top 8 shown on website)
+                $.each(data.keywords, append_keywords); //ajax result contains a dict of keywords:scores (top 8 shown on website)
             }else{
                 $("#keyword_error").html(data.error);
             }
@@ -34,18 +34,18 @@ function show_keywords() {
     var title = $("<p style='font-weight:bold; font-size:1.1em;'>Suggested keywords:</p>");
     keyword_list.append(title);
     for (var i = 0; i < 8; i++) {
-        if (suggested_keywords[i] == "" || suggested_keywords[i] == null) {
-            return null;
+        if (suggested_keywords[i] == null) {
+            return null; //break if there are no more suggested keywords available
         }
         var new_keyword_element = $("<div class='keyword'></div>");
         var buttons = $("<div class='keyword_buttons'></div>");
-        var add_button = $("<div class='add_keyword glyphicon glyphicon-ok' data-value='keyword' title='Add this keyword to list' onclick='send_user_opinion(this,\"approved\")' style='color:green; cursor:pointer;'></div>");
-        var discard_button = $("<div class='discard_keyword glyphicon glyphicon-remove' data-value='keyword' title='Discard this keywors' onclick='send_user_opinion(this,\"discarded\")' style='color:red; cursor:pointer;'></div>");
+        var add_button = $("<div class='add_keyword glyphicon glyphicon-ok' data-value="+suggested_keywords[i][1]+" title='Add this keyword to list' onclick='send_user_opinion(this,\"approved\")' style='color:green; cursor:pointer;'></div>");
+        var discard_button = $("<div class='discard_keyword glyphicon glyphicon-remove' data-value="+suggested_keywords[i][1]+" title='Discard this keywors' onclick='send_user_opinion(this,\"discarded\")' style='color:red; cursor:pointer;'></div>");
         buttons.append(discard_button);
         buttons.append(add_button);
         new_keyword_element.append(buttons);
 
-        var keyword_text = $("<p>" + suggested_keywords[i] + "</p>");
+        var keyword_text = $("<p>" + suggested_keywords[i][0] + "</p>");
         new_keyword_element.append(keyword_text);
 
         keyword_list.append(new_keyword_element)
@@ -65,8 +65,9 @@ function add_keyword(div) {
     $(div).parent().parent().remove();
     user_keywords.push(keyword);
     suggested_keywords = suggested_keywords.filter(function (e) {
-        return e !== keyword
+        return e[0] !== keyword;
     })
+    console.log(suggested_keywords);
     show_keywords();
 };
 
@@ -74,16 +75,20 @@ function discard_keyword(div) {
     var keyword = $(div).parent().parent().find("p").text();
     $(div).parent().parent().remove();
     suggested_keywords = suggested_keywords.filter(function (e) {
-        return e !== keyword
+        return e[0] !== keyword;
     })
+    console.log(suggested_keywords);
     show_keywords();
 };
 
-function append_keywords(index, keyword) {
-    if (suggested_keywords.includes(keyword) || user_keywords.includes(keyword)) {
+function append_keywords(keyword, score) {
+    if (suggested_keywords.includes([keyword,score]) || user_keywords.includes(keyword)) {
         return null;
     }
-    suggested_keywords.push(keyword);
+    suggested_keywords.push([keyword,score]);
+    suggested_keywords = suggested_keywords.sort(function(a,b) {
+        return b[1] - a[1];
+    });
     show_keywords();
 };
 
