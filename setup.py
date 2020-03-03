100d96e0cca341c030d6f405a762966d7dbc050a
oyvi96 <o>
2020-03-03 17:10:19 +0100

    Removed not working changes in setup

--- setup.py
+++ setup.py
@@ -5,11 +5,6 @@ from setuptools import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-from arxivdigest.frontend import app
-
-app.js_bundle.build()
-app.css_bundle.build()
-
 setup(
     name='arxivdigest',
     version='1.0',
@@ -17,9 +12,6 @@ setup(
     package_data={'arxivdigest.core.mail': ['templates/*.tmpl'],
                   'arxivdigest.frontend': ['templates/*.html',
                                            'static/*',
-                                           'static/.webassets-cache/*',
-                                           'static/gen/js/*',
-                                           'static/gen/css/*',
                                            'static/icons/*',
                                            'static/javascript/*'
                                            ],
