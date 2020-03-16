2934b96192a7cdedd027a4d5e2041376eae35850
oyvi96 <o>
2020-03-16 16:52:10 +0100

    closes #104

--- arxivdigest/core/mail/mail_server.py
+++ arxivdigest/core/mail/mail_server.py
@@ -6,7 +6,7 @@ import smtplib
 from email.mime.text import MIMEText
 
 from jinja2 import Environment
-from jinja2 import FileSystemLoader
+from jinja2 import PackageLoader
 
 
 def assemble_mail(message, to_address, from_address, subject):
@@ -20,13 +20,13 @@ def assemble_mail(message, to_address, from_address, subject):
 
 class MailServer:
 
-    def __init__(self, from_address, password, host, port, templates):
+    def __init__(self, from_address, password, host, port):
         """Starts and logs into mail server"""
         self.from_address = from_address
         self.server = smtplib.SMTP(host, port)
         self.server.starttls()
         self.server.login(self.from_address, password)
-        self.env = Environment(loader=FileSystemLoader(templates))
+        self.env = Environment(loader=PackageLoader(__name__, 'templates'))
 
     def close(self):
         self.server.quit()
